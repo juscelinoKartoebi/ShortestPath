35a634b440ee15d64f53f1517aa9b618f8d1960e
juscelinokartoebi <jkartoebi@gmail.com>
2021-09-09 11:52:20 -0300

    commit

--- src/sr/bludots/search/DistPar.java
+++ src/sr/bludots/search/DistPar.java
@@ -5,10 +5,10 @@ package sr.bludots.search;
  * en dmv van een dijkstra algoritme de shortest route gaat zoeken, opslaan en uit kan printen.
  */
 
-class DistanceParent {              // class of distance and parent
-                                    // items stored in sPath array (sPath array= shortest path [])
-    public int distancePar;         // distance from start to the vertex
-    public int parentVert;          // current parent of the vertex
+class DistanceParent {              
+                                    // items worden opgeslagen in de sPath array(sPath array= shortest path[])
+    public int distancePar;         // distance van start naar vertex
+    public int parentVert;          // current parent naar vertex
 
     public DistanceParent(int parentV, int distPar) {
         distancePar = distPar;
@@ -28,14 +28,14 @@ isInTree = false;
 
 class Graph {
     private final int MAX_VERTS = 20;       // Max size 
-    private final int INFINITY = 1000000;   // if a vertex is infinity, it means it is unreachable
-    private Vertex vertexList[];            // list of vertices
+    private final int INFINITY = 1000000;   // als een vertex infinity is, betekent t dat hij unreachable is
+    private Vertex vertexList[];            // lijst met vertices
     private int adjMat[][];                 // adjacency matrix
-    private int nVerts;                     // current number of vertices
-    private int nTree;                      // number of vertices in tree
-    private DistanceParent sPath[];         // array for shortest-path data
+    private int nVerts;                     // current aantal vertices
+    private int nTree;                      // aantal vertices in tree
+    private DistanceParent sPath[];         // array voor shortest-path data
     private int currentVert;                // current vertex
-    private int startToCurrent;             // distance to currentVert
+    private int startToCurrent;             // distance naar currentVert
 
     public Graph() {                           
         vertexList = new Vertex[MAX_VERTS];    
@@ -43,91 +43,90 @@ class Graph {
         nVerts = 0;
         nTree = 0;
 
-        for (int j = 0; j < MAX_VERTS; j++)         // set adjacency
+        for (int j = 0; j < MAX_VERTS; j++)         // zet adjacency
             for (int k = 0; k < MAX_VERTS; k++)     // matrix
-                adjMat[j][k] = INFINITY;            // to infinity
+                adjMat[j][k] = INFINITY;            // naar infinity
         sPath = new DistanceParent[MAX_VERTS];      // shortest paths
 
     }
 
-    public void addVertex(String city){
+    public void addVertex(String city){       //je add je vertices
         vertexList[nVerts++] = new Vertex(city);
     }
 
     public void addEdge(int start, int end, int weight){
-        adjMat[start][end] = weight;     // (can only go one way, not backwards)
-    }
+        adjMat[start][end] = weight;     // (geeft aan dat je alleen van start naar end kan gaan en niet terug)
+    }                                    // dus directed graph
 
-    public void findShortestPath(int userStartTree) {   // find all shortest paths
+    public void findShortestPath(int userStartTree) {   // zoek alle shortest paths
         int startTree = userStartTree;
       
         vertexList[startTree].isInTree = true;
-        nTree = 1;                                 // put it in tree
+        nTree = 1;                                 // if true, zet in tree
 
-        // transfer row of distances from adjMat to sPath
+        // rij met distances transferen van adjMat naar sPath
         for (int j = 0; j < nVerts; j++) {
             int tempDist = adjMat[startTree][j];
             sPath[j] = new DistanceParent(startTree, tempDist);
         }
-        // until all vertices are in the tree
+        // als alle vertices al in the tree zijn
         while (nTree < nVerts) {
-            int indexMin = getMin();                   // get minimum from sPath
+            int indexMin = getMin();                   // get minimum van sPath
             int minDist = sPath[indexMin].distancePar;
-            if (minDist == INFINITY)                   // if all infinite
-            {                                          // or in tree,
+            if (minDist == INFINITY)                   // als allemaal infinite zijn
+            {                                          // of in tree,
                 System.out.println("There are unreachable vertices");
-                break;                                 // sPath is complete
-            } else {                                   // reset currentVert to closest vertex
+                break;                                 // sPath is dan compleet
+            } else {                                   // reset currentVert naar closest vertex
                 currentVert = indexMin;                
                 startToCurrent = sPath[indexMin].distancePar;
-                // minimum distance from startTree is
-                // to currentVert, and is startToCurrent
+                // minimum distance van startTree is
+                // naar currentVert, en is startToCurrent
             }
-            // put current vertex in tree
+            // zet current vertex in tree
             vertexList[currentVert].isInTree = true;
             nTree++;
             adjust_sPath_shortest();                    // update sPath[] array
-        }                                               // end while(nTree<nVerts)
-        displayShortestPaths();                                 // display sPath[] contents
-        nTree = 0;                                      // clear tree
+        }                                              
+        nTree = 0;                                      // delete tree
         for (int j = 0; j < nVerts; j++)
             vertexList[j].isInTree = false;
     }
 
-    public int getMin() {                               // get entry from sPath with minimum distance
-        int minDist = INFINITY;                         // assume minimum
+    public int getMin() {                               // get entry van sPath met minimum distance
+        int minDist = INFINITY;                         //  minimum
         int indexMin = 0;
 
-        for (int j = 1; j < nVerts; j++) {              // for each vertex, if it's in tree and smaller than old one
+        for (int j = 1; j < nVerts; j++) {              // voor elke vertex, als het in tree is en kleiner dan oude
             if (!vertexList[j].isInTree && sPath[j].distancePar < minDist) {
                 minDist = sPath[j].distancePar;
                 indexMin = j;                           // update minimum
             }
-        }                                               // end for
-        return indexMin;                                // return index of minimum
+        }                                           
+        return indexMin;                                // return index van minimum
     }
 
-    public void adjust_sPath_shortest() {         // adjust values in shortest-path array sPath
+    public void adjust_sPath_shortest() {         
         int column = 1;                           // skip starting vertex
 
-        while (column < nVerts)                   // go across columns
+        while (column < nVerts)                   
         {
             // if this column vertex already in tree, skip it
             if (vertexList[column].isInTree) {
                 column++;
                 continue;
             }
-            // calculate distance for one sPath entry
-            // get edge from currentVert to column
+            // calculate distance van sPath entry
+            // get edge van currentVert naar column
             int currentToFringe = adjMat[currentVert][column];
 
-            // add distance from start
+            // add distance van start
             int startToFringe = startToCurrent + currentToFringe;
 
-            // get distance of current sPath entry
+            // get distance van current sPath entry
             int sPathDist = sPath[column].distancePar;
 
-            // compare distance from start with sPath entry
+            // vergelijk distance van start met sPath entry
             if (startToFringe < sPathDist)                         // if shorter,
             {
                 // update sPath
@@ -139,7 +138,7 @@ class Graph {
     }                                                        
 
     public void displayShortestPaths() {
-        for (int j = 0; j < nVerts; j++)                          // display contents of sPath[]
+        for (int j = 0; j < nVerts; j++)                          // display inhoud van sPath[]
         {
             System.out.print(vertexList[j].stad + "=");           // stad=
             if (sPath[j].distancePar == INFINITY)
